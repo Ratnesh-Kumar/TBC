0b1fcf71cfa1673777aaa95c8ffc95221f775ec3
jaideep012 <Jaideep.singh@officedepot.com>
2020-05-25 17:05:17 +0530

    calendar event header added

--- src/scenes/More/calenderEvent.js
+++ src/scenes/More/calenderEvent.js
@@ -2,62 +2,64 @@ import React from 'react';
 import { Dimensions, View, Text, Alert } from 'react-native';
 import firebase from 'react-native-firebase';
 import EventCalendar from '../../components/EventCalender/EventCalender';
+import Header from '../../components/Header';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import * as AddCalendarEvent from 'react-native-add-calendar-event';
 import { db } from '../../config/db';
 let { width } = Dimensions.get('window');
 import moment from 'moment';
+var moreConstants = require('./moreConstants');
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      eventsTest:[
+      eventsTest: [
         {
-          start:'',end:'',title:'',summary:''
+          start: '', end: '', title: '', summary: ''
         }
       ],
-      
+
     };
   }
-    componentDidMount=()=>{
-    let that=this;
-      const uid = 'CM7BJ7OyWXhGyDwLX1DMqabWfY42';
-      db.ref('/appointmentData').child(uid).on('value', function (snapshot) {
-        if (snapshot.exists()){
+  componentDidMount = () => {
+    let that = this;
+    const uid = 'CM7BJ7OyWXhGyDwLX1DMqabWfY42';
+    db.ref('/appointmentData').child(uid).on('value', function (snapshot) {
+      if (snapshot.exists()) {
         let data = snapshot.val();
         let eventsTest = Object.values(data);
         console.log(eventsTest);
-        that.setState({eventsTest});
-        }
-        else{
-          
-        }
+        that.setState({ eventsTest });
+      }
+      else {
+
+      }
 
     });
-      
-    }
-    
-    showCalendarEventWithId = (eventId) => {
-      const eventConfig = {
-        eventId,
-        allowsEditing: false,
-        allowsCalendarPreview: true,
-        navigationBarIOS: {
-          tintColor: 'orange',
-          backgroundColor: 'green',
-        },
-      };
-  
-      AddCalendarEvent.presentEventViewingDialog(eventConfig)
-        .then(eventInfo => {
-          //alert('eventInfo -> ' + JSON.stringify(eventInfo));
-        })
-        .catch((error) => {
-          alert('Error -> ' + error);
-        });
+
+  }
+
+  showCalendarEventWithId = (eventId) => {
+    const eventConfig = {
+      eventId,
+      allowsEditing: false,
+      allowsCalendarPreview: true,
+      navigationBarIOS: {
+        tintColor: 'orange',
+        backgroundColor: 'green',
+      },
     };
-  
-  
+
+    AddCalendarEvent.presentEventViewingDialog(eventConfig)
+      .then(eventInfo => {
+        //alert('eventInfo -> ' + JSON.stringify(eventInfo));
+      })
+      .catch((error) => {
+        alert('Error -> ' + error);
+      });
+  };
+
+
 
   _eventTapped(event) {
     this.showCalendarEventWithId(event.eventId);
@@ -68,22 +70,23 @@ export default class App extends React.Component {
     firebase.analytics().setCurrentScreen('More');
     console.log(this.state.eventsTest)
     return (
+      <View style={{ flex: 1}}>
+        <Header title={moreConstants.CALENDAR_SCREEN} />
+        <View style={{ flex: 1, marginTop: 20 }}>
+          <EventCalendar
+            eventTapped={this._eventTapped.bind(this)}
+            events={this.state.eventsTest}
+            width={width}
+            initDate={moment().utcOffset('+05:30').format('YYYY-MM-DD')}
+            scrollToFirst
+            upperCaseHeader
+            uppercase
+            scrollToFirst={false}
 
-      <View style={{ flex: 1, marginTop: 20 }}>
-        <EventCalendar
-          eventTapped={this._eventTapped.bind(this)}
-          events={this.state.eventsTest}
-          width={width}
-          initDate={moment().utcOffset('+05:30').format('YYYY-MM-DD')}
-          scrollToFirst
-          upperCaseHeader
-          uppercase
-          scrollToFirst={false}
-          
-        />
-       
+          />
+
+        </View>
       </View>
-
     );
   }
 }
--- src/scenes/More/moreConstants.js
+++ src/scenes/More/moreConstants.js
@@ -1,2 +1,3 @@
 export const MORE_SCREEN = "More Screen";
+export const CALENDAR_SCREEN = 'Calendar Screen';
 export const CALENDAR = 'Choose Date';
